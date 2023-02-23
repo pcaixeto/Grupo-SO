46659b8986a3ed2775604e0d917dbcede64474e9
Paloma Bezerra <60986916+palomovisk@users.noreply.github.com>
2023-02-22 23:56:08 -0300

    alterando exercicio4

--- Tarefa01/exercicio4.c
+++ Tarefa01/exercicio4.c
@@ -4,18 +4,18 @@
 #include <stdlib.h>
 
 int main(){
-    char palavra1[10] = "pedro";
-    char palavra2[10] = "eduardo";
+    char nome1[10] = "pedro";
+    char nome2[10] = "eduardo";
     
     int retorno;
         
-    retorno = strcoll(palavra2, palavra1);
+    retorno = strcoll(nome2, nome1);
     if (retorno > 0) {
-        printf("Em ordem alfabetica temos \n %s \n %s \n\n", palavra1,palavra2);
+        printf("Em ordem alfabetica temos \n %s \n %s \n\n", nome1,nome2);
     } else if (retorno < 0) {
-        printf("Em ordem alfabética temos \n %s \n %s \n\n", palavra2,palavra1);
+        printf("Em ordem alfabética temos \n %s \n %s \n\n", nome2,nome1);
     } else {
-        printf("As palavras são iguais \n %s \n %s \n", palavra1, palavra2);
+        printf("As palavras são iguais \n %s \n %s \n", nome1, nome2);
     }
     
 }
