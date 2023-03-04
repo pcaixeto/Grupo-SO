ec0952a02209dbf0ea796d5f03423bec8ce75c06
Ubuntu <ubuntu@ip-172-31-15-187.ec2.internal>
2023-03-04 20:08:17 +0000

    ex2 feito

---
+++ tarefa02/ex2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+        FILE *fileA, *fileB;
+        char nameA[10], nameB[10];
+        printf("File to be copied from: ");
+        scanf("%s", nameA);
+
+        fileA = fopen(nameA, "r");
+        if(fileA == NULL){
+                printf("\nThe file cannot be opened. Exiting program.\n");
+                return 1;
+        }else
+                printf("\nFile opened successfully.\n");
+
+        printf("\nNew file name: ");
+        scanf("%s",nameB);
+
+        fileB = fopen(nameB,"w");
+        if(fileB == NULL){
+                printf("\nThe file cannot be created. Exiting program.\n");
+        }else
+                printf("\nFile created successfully.\n");
+
+        char c;
+
+        c = fgetc(fileA);
+        while(c!=EOF){
+                fputc(c, fileB);
+                c = fgetc(fileA);
+        }
+
+        fclose(fileA);
+        fclose(fileB);
+
+        return 0;
+}
