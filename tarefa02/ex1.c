f36f6e975dedbf011a4febe604fb49435cc6fd03
Ubuntu <ubuntu@ip-172-31-15-187.ec2.internal>
2023-03-04 17:55:07 +0000

    teste tarefa 2

---
+++ tarefa02/ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	FILE *text;
+	text = fopen("text.txt", "r");
+	char c;
+
+	if(text == NULL){
+		printf("File cannot be  opened.");
+	}
+	
+	printf("\nPrinting file content: \n");
+
+	do{
+		c = fgetc(text);
+		printf("%c",c);	
+	}while(c!=EOF);
+
+	fclose(text);;
+
+}
