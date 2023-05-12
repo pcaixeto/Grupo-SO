3b809ec915cb09ee86279bab44e3c5850e368b4d
Arthur <arthurferreira.dev@gmail.com>
2023-05-12 15:05:20 -0300

    criada 2 funcoes transferencia + um semaforo p cada funcao

--- Trabalho02/trabalho.c
+++ Trabalho02/trabalho.c
@@ -20,71 +20,68 @@ typedef struct c conta;
 conta from, to;
 int valor;
 conta * p, * q;
-int i = 0;
 int cont = 0;
-sem_t mutex;
+sem_t mutexF;
+sem_t mutexT;
 
-
-void *transferencia(void* thread) {
-
-    while (to.saldo > 0 && from.saldo > 0 /*&& cont <100*/) {
-		i++;
-		sem_wait(&mutex);
-        if (i % 5 == 0) {
-            if (from.saldo >= valor && from.saldo > 0) { 
-				cont++;// 2
-                from.saldo -= valor;
-                to.saldo += valor;
-                printf("Transferência concluída com sucesso!\n");
-                printf("Saldo de c1: %d\n", from.saldo);
-                printf("Saldo de c2: %d\n", to.saldo);
-            }
-        } else {
-            if (to.saldo >= valor && to.saldo > 0) {
-				cont++;
-                to.saldo -= valor;
-                from.saldo += valor;
-                printf("Transferência concluída com sucesso!\n");
-                printf("Saldo de c1: %d\n", from.saldo);
-                printf("Saldo de c2: %d\n", to.saldo);
-            }
-        }
-		sem_post(&mutex);
-    }
+void *transferenciaC1(void* thread){
+	if(from.saldo > 0 && to.saldo>0){
+		sem_wait(&mutexF);
+		from.saldo -= valor;
+		to.saldo += valor;
+		printf("\ntransferindo da conta 1 para conta 2");
+		printf("\nsaldo de c1: %d", from.saldo);
+		printf("\nsaldo de c2: %d", to.saldo);
+		sem_post(&mutexF);
+	}
 
 	return NULL;
+}
+
+void *transferenciaC2(void* thread) {
+	if(to.saldo > 0 && from.saldo>0){
+		sem_wait(&mutexT);
+                to.saldo -= valor;
+                from.saldo += valor;
+                printf("\ntransferindo da conta 2 para conta 1");
+                printf("\nsaldo de c1: %d", from.saldo);
+                printf("\nsaldo de c2: %d", to.saldo);
+		sem_post(&mutexT);
+        }
+
+        return NULL;
+
 
 }
 
 int main() {
-    int threadCount = 100;
-    void * stack;
-    pid_t pid;
-    intptr_t i;
+    	int threadCount = 100;
+    	void * stack;
+    	intptr_t i;
 
-    pthread_t threadIds[threadCount];
+	pthread_t threadIds[threadCount];
 
-	sem_init(&mutex, 0, 1);
+	sem_init(&mutexF, 0, 1);
+	sem_init(&mutexT, 0, 1);
    
-    stack = malloc(FIBER_STACK);
-    if (stack == 0) {
-        perror("malloc: could not allocate stack");
-        exit(1);
-    }
-    from.saldo = 10000;
-    to.saldo = 10000;
+   
+    	from.saldo = 10000;
+    	to.saldo = 10000;
 
-    valor = 10;
-    printf("Transferindo %d para a conta c2\n", valor);;
-    for (i = 0; i < threadCount; i++) {
-        pthread_create( &threadIds[i], NULL, transferencia, (void*) i);
-    }
+    	valor = 10;
+    	
+    	for (i = 0; i < threadCount; i++) {
+		if(i%2==0)
+        		pthread_create( &threadIds[i], NULL, transferenciaC2, (void*) i);
+		else
+			pthread_create( &threadIds[i], NULL, transferenciaC1, (void*) i);
+    	}
 
-    for (i = 0; i < threadCount; i++) {
-        pthread_join(threadIds[i], NULL);
-    }
+    	for (i = 0; i < threadCount; i++) {
+        	pthread_join(threadIds[i], NULL);
+    	}
 
-    free(stack);
-    printf("Transferências concluídas e memória liberada.\n");
-    return 0;
+   	 free(stack);
+   	 printf("Transferências concluídas e memória liberada.\n");
+   	 return 0;
 }
